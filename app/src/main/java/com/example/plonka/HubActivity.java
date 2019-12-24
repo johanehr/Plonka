aa2d4130d2b79fc8ba795dffefc1984447e1adc6
Johan Ehrenfors <luhcforgh@gmail.com>
2019-12-25 00:12:43 +0100

    Add contact options, drawer icons

--- app/src/main/java/com/example/plonka/HubActivity.java
+++ app/src/main/java/com/example/plonka/HubActivity.java
@@ -1,8 +1,10 @@
 package com.example.plonka;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.example.plonka.data.model.LoggedInUser;
@@ -24,10 +26,12 @@ import com.google.android.material.snackbar.Snackbar;
 
 import android.os.Looper;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -47,7 +51,7 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class HubActivity extends AppCompatActivity {
+public class HubActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private AppBarConfiguration mAppBarConfiguration;
 
@@ -81,13 +85,17 @@ public class HubActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
 
+        // Set up custom intents for drawer menu buttons
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // Set up user profile at top
         View headerView = navigationView.getHeaderView(0);
         TextView textUserName = (TextView) headerView.findViewById(R.id.textViewUserName);
         textUserName.setText(userName);
         TextView textUserId = (TextView) headerView.findViewById(R.id.textViewUserId);
         textUserId.setText("User ID: "+userId);
 
-        Log.i(LOG_TAG, "Set up navigation");
+        Log.i(LOG_TAG, "Setup complete");
     }
 
     @Override
@@ -96,6 +104,44 @@ public class HubActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    // From: https://stackoverflow.com/questions/42297381/onclick-event-in-navigation-drawer
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // TODO: Fix so that all clicks work, broke legacy. Use onClick() for menu item instead? https://developer.android.com/guide/topics/ui/menus.html#java 
+        // Handle navigation view item clicks here.
+        switch (item.getItemId()) {
+
+            case R.id.nav_support_email: {
+                Log.d(LOG_TAG, "Pressed nav_support_email");
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.setType("text/plain");
+                email.putExtra(Intent.EXTRA_EMAIL, new String[] {"support@plonka.se"});
+                email.putExtra(Intent.EXTRA_SUBJECT, "Plonka support: <ENTER TITLE>");
+                email.putExtra(Intent.EXTRA_TEXT, "User ID: "+currentUser.getAccountId()+"\n\n<ENTER YOUR SUPPORT ISSUE HERE>");
+
+                if (email.resolveActivity(getPackageManager()) != null) {
+                    startActivity(Intent.createChooser(email, "Choose your e-mail client:"));
+                }
+                break;
+            }
+            case R.id.nav_support_phone: {
+                Log.d(LOG_TAG, "Pressed nav_support_phone");
+                Intent call = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:+46701234567"));
+                if (call.resolveActivity(getPackageManager()) != null) {
+                    startActivity(call);
+                }
+                break;
+            }
+            default: {
+                return super.onOptionsItemSelected(item);
+            }
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
 }
